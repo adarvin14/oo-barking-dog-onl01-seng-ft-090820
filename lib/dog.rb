628800dab9e49e42713fd8283a368712cbfae035
Alex Darvin <amdarvin14@gmail.com>
2020-09-18 23:24:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,16 @@
 # Your code goes here!
 class Dog
   
-def name=(dog_name)
+def name=(dogs_name)
+  @this_dogs_name
   
   
+  
+  
+  
+fido = Dog.new 
+fido.name = ("Fido")
+
+fido.name
+
+fido.bark
